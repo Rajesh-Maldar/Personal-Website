8f879759077f93fd9d57ca199288393220b2ec3f
Rajesh Maldar <rajesh.maldar@tech.jago.com>
2024-08-10 18:48:18 +0530

    adding social media icons and links

--- my-app/src/components/Main.tsx
+++ my-app/src/components/Main.tsx
@@ -1,15 +1,40 @@
 import React from "react";
 import videoBg from "../assets/videoBg.mp4";
+import {
+  FacebookFilled,
+  InstagramFilled,
+  LinkedinFilled,
+  XOutlined,
+} from "@ant-design/icons";
+import { Space } from "antd";
 
 const Main = () => {
   return (
-    <div className="main">
-      <div className="overlay"></div>
-      <video src={videoBg} autoPlay loop muted />
-      <div className="content">
-        <h1>Greetings, and Welcome Aboard</h1>
+    <>
+      <div className="main">
+        <div className="overlay"></div>
+        <video src={videoBg} autoPlay loop muted />
+        <div className="content">
+          <h1>Greetings, and Welcome Aboard</h1>
+          <div className="social-media-icons-white footer">
+            <Space>
+              <a href="https://www.facebook.com/rajesh.maldar">
+                <FacebookFilled />
+              </a>
+              <a href="https://x.com/rajeshmaldar">
+                <XOutlined />
+              </a>
+              <a href="https://www.instagram.com/rajeshmaldar?igsh=ZHpzdmt1MDNiN2sx">
+                <InstagramFilled />
+              </a>
+              <a href="https://www.linkedin.com/in/rajesh-maldar?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">
+                <LinkedinFilled />
+              </a>
+            </Space>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
